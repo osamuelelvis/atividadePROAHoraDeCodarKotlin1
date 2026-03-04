954abbccb9fc74a5ae097c647f3f6b1e48d1d22a
Samuel Elvis <169075141+osamuelelvis@users.noreply.github.com>
2026-03-04 11:22:43 -0300

    Update Main.kt

--- 001NomeFusca/src/main/kotlin/Main.kt
+++ 001NomeFusca/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+// Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário.
 fun main() {
     var name = "Fusca"
     print("Nome do carro: $name")
