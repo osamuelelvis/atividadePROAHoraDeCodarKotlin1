9d041c152557e65bce630e35dc3a5a402d1aa2d6
Samuel Elvis <169075141+osamuelelvis@users.noreply.github.com>
2026-03-04 11:23:35 -0300

    Update Main.kt

--- 003NomeEIdade/src/main/kotlin/Main.kt
+++ 003NomeEIdade/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+// Escreva um programa em que o usuário informe o seu nome e em seguida o programa perguntará a idade do usuário. Agora o programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]".
 fun main() {
     print("Digite seu nome: ")
     var nomeUsuario = readln()
